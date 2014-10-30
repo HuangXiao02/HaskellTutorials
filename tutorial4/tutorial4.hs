215e527fe9510ec5d84731a1c5f9015ed1b08345
William Mathewson <wncmathewson@me.com>
2014-10-30 17:27:37 +0000

    Uncommented Lisa's solution since it works

--- tutorial4/tutorial4.hs
+++ tutorial4/tutorial4.hs
@@ -125,12 +125,12 @@ split sep str
   | otherwise = str : []
 
 reconstruct :: String -> [String] -> String
-reconstruct _ [] = []
-reconstruct [] str = concat str
-reconstruct sep (s:str) = s ++ sep ++ reconstruct sep str
+-- reconstruct _ [] = []
+-- reconstruct [] str = concat str
+-- reconstruct sep (s:str) = s ++ sep ++ reconstruct sep str
 
 -- Lisa's solution (works well, gg)
--- reconstruct sep str = (foldr (++) "" (map (\x -> x ++ sep) (init str))) ++ (last str)
+reconstruct sep str = (foldr (++) "" (map (\x -> x ++ sep) (init str))) ++ (last str)
 
 prop_split :: Char -> String -> String -> Bool
 prop_split c sep str = reconstruct sep' (split sep' str) `sameString` str
